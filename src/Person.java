d4757c7d2f18f73811ee48a293fcd0c82c762f8b
LiselotteEL <lise1066@edu.easj.dk>
2017-11-30 10:31:59 +0100

    Dette er en test, som ikke skal bruges til noget

--- src/Person.java
+++ src/Person.java
@@ -13,4 +13,8 @@ public class Person {
     //Vores Person-klasse:
     //Hvor vi håndterer Data (fra TeamL_DB) som skal indeholde info om personer.
     //Customers, Employee, Administrator, etc..
+
+
+
+    //DETTE ER EN TEST
 }
